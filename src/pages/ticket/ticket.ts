6872b2ac10a66d3a272670394134b3e3f9c4b730
Borys Korobeinikov <korobeinikov@outlook.com>
2017-03-17 13:59:20 +0200

    styled datepicker

--- src/pages/ticket/ticket.ts
+++ src/pages/ticket/ticket.ts
@@ -32,12 +32,13 @@ export class TicketPage {
 
     public movie: Movie;
 
-    public technologies: { id: string, title: string }[];
-    public selectedTechId: string;
-
-    public dates: { id: string, value: moment.Moment }[];
+    public dates: { id: number, value: moment.Moment }[];
     public selectedDate: moment.Moment;
-    public selectedDateId: string;
+    public selectedDateId: number;
+
+    public technologies: { id: string, value: string }[];
+    public selectedTechId: string;
+    public selectedTech: string;
 
     public times: { id: string, value: moment.Moment, showtime: MovieShowtime }[];
     public selectedTimeId: string;
@@ -86,59 +87,76 @@ export class TicketPage {
             var showtimes = res.filter(s => s.movieId == this.movie.id);
             this.showtimes = showtimes;
 
-            this.technologies = _.chain(this.showtimes).map(s => s.techId).uniq().map((t, index) => ({
-                id: index + '',
-                title: t
-            })).value();
-
-            this.selectedTechId = this.technologies[0].id;
-            this.onTechChange();
+            this.onShowtimesChange();
+            return;
         });
+    }
+
+    private onShowtimesChange() {
+        var dates = _.chain(this.showtimes).map(s => ({
+            id: moment(s.time).startOf('date').valueOf(),
+            value: s.time,
+        })).uniqBy(d => d.id).value();
+
+        this.dates = dates;
+        this.selectedDate = this.dates[0].value;
+        this.selectedDateId = this.dates[0].id;
+        this.onDateChange();
+    }
+
+    onDateChange() {
+        if (this.dates == null)
+            return;
+
+        var selectedDate = this.dates.find(d => d.id == this.selectedDateId);
+        this.selectedDate = selectedDate.value;
+
+        var techs = _.chain(this.showtimes)
+            .filter(s => s.time.isSame(selectedDate.value, 'day'))
+            .map(s => ({
+                id: s.techId,
+                value: s.techId
+            }))
+            .uniqBy(t => t.id)
+            .value();
+
+        this.technologies = techs;
+        this.selectedTechId = this.technologies[0].id;
+        this.selectedTech = this.technologies[0].value;
+        this.onTechChange();
     }
 
     onTechChange() {
         try {
             var selectedTech = this.technologies.find(t => t.id == this.selectedTechId);
-            var dates = _.chain(this.showtimes).filter(s => s.techId == selectedTech.title).map(s => ({
-                id: s.time.format('ddd D'),
-                value: s.time,
-            })).uniqBy(d => d.id).value();
+            this.selectedTech = selectedTech.value;
 
-            this.dates = dates;
-            this.datepicker.update();
-            this.selectedDate = this.dates[0].value;
-            this.selectedDateId = this.dates[0].id;
-            this.onDateChange();
+            console.log(this.selectedDate);
+
+            var times = _.chain(this.showtimes)
+                .filter(s => s.time.isSame(this.selectedDate, 'day') && s.techId == this.selectedTechId)
+                .map(s => ({
+                    id: s.time.format("HH:mm") + '_' + s.hallId,
+                    value: s.time,
+                    showtime: s
+                }))
+                .uniqBy(t => t.id)
+                .value();
+            console.log('filtered', times);
+
+            this.times = times;
+            this.selectedTimeId = this.times[0].id;
+            this.selectedTime = this.times[0].value;
+            this.onTimeChange();
         }
         catch (err) {
 
         }
     }
 
-    onDateChange() {
-        if (this.technologies == null || this.dates == null)
-            return;
-
-        var selectedTech = this.technologies.find(t => t.id == this.selectedTechId);
-        var selectedDate = this.dates.find(d => d.id == this.selectedDateId);
-        this.selectedDate = selectedDate.value;
-
-        var times = _.chain(this.showtimes)
-            .filter(s => s.techId == selectedTech.title && s.time.isSame(selectedDate.value, 'day'))
-            .map(s => ({
-                id: s.time.format("HH:mm") + '_' + s.hallId,
-                value: s.time,
-                showtime: s
-            }))
-            .value();
-
-        this.times = times;
-        this.selectedTimeId = this.times[0].id;
-        this.selectedTime = this.times[0].value;
-        this.onTimeChange();
-    }
-
     onTimeChange() {
+        if (this.times == null)
+            return;
         var selectedTime = this.times.find(t => t.id == this.selectedTimeId);
         this.selectedTime = selectedTime.value;
     }
