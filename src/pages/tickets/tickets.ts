a471c17e3a60fbb481b78479914369432a1f4c4c
Borys Korobeinikov <korobeinikov@outlook.com>
2017-03-26 17:24:44 +0300

    tickets page ui

--- src/pages/tickets/tickets.ts
+++ src/pages/tickets/tickets.ts
@@ -1,34 +1,59 @@
-import { Component, ChangeDetectionStrategy, NgZone } from '@angular/core';
+import { Component, ChangeDetectionStrategy, NgZone, OnDestroy, OnInit } from '@angular/core';
 import { App } from "ionic-angular";
 
 import { Store } from "@ngrx/store";
 import { State } from "./../../store";
-import { Ticket } from './../../store/models';
+import { Ticket, Movie } from './../../store/models';
 import * as selectors from "./../../store/selectors";
 import * as actionsTicket from "./../../store/actions/ticket";
 
 import { Observable } from "rxjs/Observable";
+import { Subscription } from "rxjs/Subscription";
 
 import { TicketPage } from "../ticket/ticket";
+
+import moment from 'moment';
 
 @Component({
     selector: 'page-tickets',
     templateUrl: 'tickets.html',
 })
-export class TicketsPage {
+export class TicketsPage implements OnInit, OnDestroy {
 
-    public tickets$: Observable<Ticket[]>;
-    
+    public tickets: Ticket[];
+    public movies: { [movieId: string]: Movie };
+
+    private subscription: Subscription = new Subscription();
+
     constructor(
         private appCtrl: App,
         private store: Store<State>,
-        private zone:NgZone,
+        private zone: NgZone,
     ) {
-        this.tickets$ = store.select(selectors.getTicketAll);
+    }
+
+    ngOnInit() {
+        let s = this.store.select(selectors.getTicketAll)
+            .withLatestFrom(this.store.select(selectors.getMovieEntities))
+            .subscribe(([tickets, movies]) => {
+                this.tickets = tickets;
+                this.movies = movies;
+            });
+
+        this.subscription.add(s);
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
     }
 
     onSelect(ticket: Ticket) {
         this.store.dispatch(new actionsTicket.SelectAction(ticket.id));
         this.appCtrl.getRootNav().push(TicketPage);
+    }
+
+    duration(duration: number) {
+        var d = moment.duration(duration, "minutes");
+        return d.hours() + "h " + d.minutes() + "min";
     }
 }
